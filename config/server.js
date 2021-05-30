1299b9ccabf60341f13a13c927443b3c205306e8
Carmine Milieni <carmine.milieni@gmail.com>
2021-05-30 17:32:59 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7fc0bb4f91544c6b928915305407df6c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0fedad69-7be0-4bd0-9166-d5cae886d249'
+};
