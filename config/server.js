3d53f05149d003ec5c3a80243d6fcd58cc191063
Carmine Milieni <carmine.milieni@gmail.com>
2021-05-30 20:24:21 +0200

    up

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd44250cd36b7b95a4073c0cb626bbfa1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7fa27123-8efe-4f2e-b8dd-6f0a2941b9b5'
+};
